f8b68d4e8b5c3c8bf9a7a91df1c4ef9ba5eb08fe
Tom Lazar <16lazart@elmbrookstudents.org>
2016-02-16 08:14:07 -0600

    Auton Stuff

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -17,6 +17,15 @@
 #include "Profile/DProfile.h"
 
 #define debug 1
+#define RAMPARTS 0
+#define LOWBAR 1
+#define ROCKWALL 2
+#define DEBRIS 3
+#define MOAT 4
+#define DRAW 5
+#define CHEVAL 6
+#define SALLY 7
+#define PORTI 8
 
 struct nLNode {
 	IControl* value;
@@ -110,112 +119,225 @@ public:
 		//MUST BE CALLED LAST
 		master->addNode(m, "Motor");
 
+		std::string autonID = profile->getValue("AUTOLIST");
 
+		auton = new std::vector<stepBase>();
 
-	std::string autonID = profile->getValue("AUTOLIST");
+		if (autonID.compare("BASIC") == 0) {
+			driveStep step1 = driveStep();
+			step1.command = stepBase::driveStraight;
+			step1.distance = 5;
+			step1.stepNum = 0;
+			step1.speed = .5;
+			auton->push_back(step1);
 
-	auton = new std::vector<stepBase>();
+			stepBase fin = stepBase();
+			fin.command = stepBase::stop;
+			fin.stepNum = 1;
+			auton->push_back(fin);
+		}
 
-	if (autonID.compare("BASIC") == 0) {
-		driveStep step1 = driveStep();
-		step1.command = stepBase::driveStraight;
-		step1.distance = 5;
-		step1.stepNum = 0;
-		step1.speed = .5;
-		auton->push_back(step1);
+		int def = profile->getInt("AUTO_DEF");
+		int pos = profile->getInt("AUTO_POS");
+		bool basic = true;
 
-		stepBase fin = stepBase();
-		fin.command = stepBase::stop;
-		fin.stepNum = 1;
-		auton->push_back(fin);
+		switch (def) {
+		case DEBRIS:
+			driveStep drive = driveStep();
+			drive.stepNum = 0;
+			drive.command = stepBase::driveStraight;
+			drive.distance = 2.0;
+			drive.speed = .75;
+			auton->push_back(drive);
+			break; //TODO
+		case RAMPARTS:
+			driveStep drive = driveStep();
+			drive.stepNum = 0;
+			drive.command = stepBase::driveStraight;
+			drive.distance = 2.0;
+			drive.speed = .75;
+			auton->push_back(drive);
+			break; //TODO
+		case LOWBAR:
+			driveStep drive = driveStep();
+			drive.stepNum = 0;
+			drive.command = stepBase::driveStraight;
+			drive.distance = 2.0;
+			drive.speed = .75;
+			auton->push_back(drive);
+			break; //TODO
+		case MOAT:
+			driveStep drive = driveStep();
+			drive.stepNum = 0;
+			drive.command = stepBase::driveStraight;
+			drive.distance = 2.0;
+			drive.speed = .75;
+			auton->push_back(drive);
+			break; //TODO
+		case ROCKWALL:
+			driveStep drive = driveStep();
+			drive.stepNum = 0;
+			drive.command = stepBase::driveStraight;
+			drive.distance = 2.0;
+			drive.speed = .75;
+			auton->push_back(drive);
+			break; //TODO
+		case PORTI:
+			driveStep drive = driveStep();
+			drive.stepNum = 0;
+			drive.command = stepBase::driveStraight;
+			drive.distance = 2.0;
+			drive.speed = .75;
+			auton->push_back(drive);
+			break; //TODO
+		default:
+			basic = false;
+			break;
+		}
+
+		switch (pos) {
+		case 1:
+			turnStep turn = turnStep();
+			turn.stepNum = 1;
+			turn.command = stepBase::turn;
+			turn.angle = 45.0;
+			turn.speed = .5;
+			auton->push_back(turn);
+
+			break; //TODO
+		case 2:
+			turn.stepNum = 1;
+			turn.command = stepBase::turn;
+			turn.angle = 45.0;
+			turn.speed = .5;
+			auton->push_back(turn);
+			break; //TODO
+		case 3:
+			turn.stepNum = 1;
+			turn.command = stepBase::turn;
+			turn.angle = 45.0;
+			turn.speed = .5;
+			auton->push_back(turn);
+			break; //TODO
+		case 4:
+			turn.stepNum = 1;
+			turn.command = stepBase::turn;
+			turn.angle = 45.0;
+			turn.speed = .5;
+			auton->push_back(turn);
+			break; //TODO
+		case 5:
+			turn.stepNum = 1;
+			turn.command = stepBase::turn;
+			turn.angle = 45.0;
+			turn.speed = .5;
+			auton->push_back(turn);
+			break; //TODO
+		default:
+			break; //TODO
+		}
+
+		stepBase prepareShot = stepBase();
+		prepareShot.stepNum = 2;
+		prepareShot.command = stepBase::target;
+		auton->push_back(prepareShot);
+
+		stepBase shoot = stepBase();
+		shoot.stepNum = 3;
+		shoot.command = stepBase::shoot;
+		auton->push_back(shoot);
+
+		stepBase stop = stepBase();
+		stop.stepNum = 4;
+		stop.command = stepBase::stop;
+		auton->push_back(stop);
+
 	}
-
-}
 
 private:
-LiveWindow *lw;
+	LiveWindow *lw;
 
-void RobotInit() {
-	lw = LiveWindow::GetInstance();
-	//SmartDashboard::PutString("Profile",robot);
-	SmartDashboard::PutString("MOTORFL", profile->getValue("MOTORFL"));
-	SmartDashboard::PutString("State", "Robot Init");
-	nLNode* test = master->head;
-	while (test != NULL) {
-		test->value->RobotInit();
-		test = test->parent;
+	void RobotInit() {
+		lw = LiveWindow::GetInstance();
+		//SmartDashboard::PutString("Profile",robot);
+		SmartDashboard::PutString("MOTORFL", profile->getValue("MOTORFL"));
+		SmartDashboard::PutString("State", "Robot Init");
+		nLNode* test = master->head;
+		while (test != NULL) {
+			test->value->RobotInit();
+			test = test->parent;
+		}
+
 	}
 
-}
-
-void AutonomousInit() {
-	SmartDashboard::PutString("State", "Autonomous Init");
-	nLNode* test = master->head;
-	while (test != NULL) {
-		test->value->AutonomousInit();
-		test = test->parent;
-	}
-}
-
-void AutonomousPeriodic() {
-	SmartDashboard::PutString("State", "Autonomous Periodic");
-	//No list here beacause auto was always a bit more complicated
-
-	int x = 0;
-	if (robot.compare("ORYX") == 0) {
-		stepBase *command = &auton->at(x);
-		if (command != NULL) {
-			bool result = sensorControl->AutonomousPeriodic(command);
-			if (result) {
-				x += 1;
-				command = &auton->at(x);
-			}
+	void AutonomousInit() {
+		SmartDashboard::PutString("State", "Autonomous Init");
+		nLNode* test = master->head;
+		while (test != NULL) {
+			test->value->AutonomousInit();
+			test = test->parent;
 		}
 	}
-	nLNode* test = master->head;
-	while (test != NULL) {
-		test->value->AutonomousPeriodic();
-		test = test->parent;
+
+	void AutonomousPeriodic() {
+		SmartDashboard::PutString("State", "Autonomous Periodic");
+		//No list here beacause auto was always a bit more complicated
+
+		int x = 0;
+		if (robot.compare("ORYX") == 0) {
+			stepBase *command = &auton->at(x);
+			if (command != NULL) {
+				bool result = sensorControl->AutonomousPeriodic(command);
+				if (result) {
+					x += 1;
+					command = &auton->at(x);
+				}
+			}
+		}
+		nLNode* test = master->head;
+		while (test != NULL) {
+			test->value->AutonomousPeriodic();
+			test = test->parent;
+		}
+
 	}
 
-}
-
-void TeleopInit() {
-	SmartDashboard::PutString("State", "Teleop Init");
-	nLNode* test = master->head;
-	while (test != NULL) {
-		test->value->TeleopInit();
-		test = test->parent;
+	void TeleopInit() {
+		SmartDashboard::PutString("State", "Teleop Init");
+		nLNode* test = master->head;
+		while (test != NULL) {
+			test->value->TeleopInit();
+			test = test->parent;
+		}
 	}
-}
 
-void TeleopPeriodic() {
-	SmartDashboard::PutString("State", "Teleop Periodic");
-	nLNode* test = master->head;
-	while (test != NULL) {
-		test->value->TeleopPeriodic();
-		test = test->parent;
+	void TeleopPeriodic() {
+		SmartDashboard::PutString("State", "Teleop Periodic");
+		nLNode* test = master->head;
+		while (test != NULL) {
+			test->value->TeleopPeriodic();
+			test = test->parent;
+		}
 	}
-}
 
-void TestInit() {
-	SmartDashboard::PutString("State", "Test Init");
-	nLNode* test = master->head;
-	while (test != NULL) {
-		test->value->TestInit();
-		test = test->parent;
+	void TestInit() {
+		SmartDashboard::PutString("State", "Test Init");
+		nLNode* test = master->head;
+		while (test != NULL) {
+			test->value->TestInit();
+			test = test->parent;
+		}
 	}
-}
 
-void TestPeriodic() {
-	SmartDashboard::PutString("State", "Test Periodic");
-	lw->Run();
-	nLNode* test = master->head;
-	while (test != NULL) {
-		test->value->TestPeriodic();
-		test = test->parent;
+	void TestPeriodic() {
+		SmartDashboard::PutString("State", "Test Periodic");
+		lw->Run();
+		nLNode* test = master->head;
+		while (test != NULL) {
+			test->value->TestPeriodic();
+			test = test->parent;
+		}
 	}
-}
 };
 
 START_ROBOT_CLASS(Robot);
